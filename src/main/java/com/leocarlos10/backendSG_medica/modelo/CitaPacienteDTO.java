020d9ac24f72d12b5fe9859bb8532f481829cb1a
leocarlos10 <leocarlosospinacausil10@gmail.com>
2025-06-12 15:25:44 -0500

    optimizando la clase CitaPacienteDTO.java

--- src/main/java/com/leocarlos10/backendSG_medica/modelo/CitaPacienteDTO.java
+++ src/main/java/com/leocarlos10/backendSG_medica/modelo/CitaPacienteDTO.java
@@ -3,80 +3,38 @@ package com.leocarlos10.backendSG_medica.modelo;
 import java.sql.Timestamp;
 import java.util.Date;
 
+import lombok.Getter;
+import lombok.Setter;
+
 public class CitaPacienteDTO {
+    @Getter @Setter
     private Long idCita;
+    @Getter @Setter
     private Timestamp fechahora;
+    @Getter @Setter
     private String servicio;
+    @Getter @Setter
     private String nombre;
+    @Getter @Setter
     private String telefono;
+    @Getter @Setter
     private Date fechaUltValoracion;
+    @Getter @Setter
     private String motivoC;
+    @Getter @Setter
     private String remitente;
 
     public CitaPacienteDTO() {
     }
 
-    public Long getIdCita() {
-        return idCita;
-    }
-
-    public void setIdCita(Long idCita) {
+    public CitaPacienteDTO(Long idCita, Timestamp fechahora, String servicio, String nombre, String telefono, Date fechaUltValoracion, String motivoC, String remitente) {
         this.idCita = idCita;
-    }
-
-    public Timestamp getFechahora() {
-        return fechahora;
-    }
-
-    public void setFechahora(Timestamp fechahora) {
         this.fechahora = fechahora;
-    }
-
-    public String getServicio() {
-        return servicio;
-    }
-
-    public void setServicio(String servicio) {
         this.servicio = servicio;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
         this.nombre = nombre;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
         this.telefono = telefono;
-    }
-
-    public Date getFechaUltValoracion() {
-        return fechaUltValoracion;
-    }
-
-    public void setFechaUltValoracion(Date fechaUltValoracion) {
         this.fechaUltValoracion = fechaUltValoracion;
-    }
-
-    public String getMotivoC() {
-        return motivoC;
-    }
-
-    public void setMotivoC(String motivoC) {
         this.motivoC = motivoC;
-    }
-
-    public String getRemitente() {
-        return remitente;
-    }
-
-    public void setRemitente(String remitente) {
         this.remitente = remitente;
     }
 }
