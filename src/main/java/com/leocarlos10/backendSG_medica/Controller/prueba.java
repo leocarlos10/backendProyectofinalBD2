ad878ceea576b08610f58be447b73e75c1449d91
Adrian Ascanio <adrianvillazon@gmail.com>
2025-06-12 16:15:15 -0500

    comprobacion de conexion DB funcion dbTest() -> /db-test | en prueba.java

--- src/main/java/com/leocarlos10/backendSG_medica/Controller/prueba.java
+++ src/main/java/com/leocarlos10/backendSG_medica/Controller/prueba.java
@@ -13,4 +13,15 @@ public class prueba {
     public List<String> getUsuarios() {
         return List.of("usuario1", "usuario2", "usuario3", "usuario4", "usuario5");
     }
+
+    @GetMapping("/db-test")
+    public String dbTest() {
+        try {
+        jdbcTemplate.queryForObject("SELECT 1", Integer.class);
+            // Si la consulta se ejecuta correctamente, significa que la conexión es exitosa
+        return "Conexión a la base de datos exitosa";
+        } catch (Exception e) {
+            return "Conexión a la base de datos fallida: " + e.getMessage();
+        }
+    }
 }
