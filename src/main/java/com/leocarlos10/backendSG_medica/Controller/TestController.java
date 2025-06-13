d45b1c38413497770085b10c343b09b7a2a9475a
Adrian Ascanio <adrianvillazon@gmail.com>
2025-06-13 13:39:09 -0500

    Creacion de endpoint para Proximas Citas usando Procedure en la base de datos

--- src/main/java/com/leocarlos10/backendSG_medica/Controller/TestController.java
+++ src/main/java/com/leocarlos10/backendSG_medica/Controller/TestController.java
@@ -1,8 +1,15 @@
 package com.leocarlos10.backendSG_medica.Controller;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import com.leocarlos10.backendSG_medica.Models.CitasPanel;
+import com.leocarlos10.backendSG_medica.conexionDAO.PanelDAO;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import com.leocarlos10.backendSG_medica.Models.CitasPanel;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 
@@ -14,6 +21,9 @@ public class TestController {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
+    @Autowired
+    private PanelDAO panelDAO;
+
     @GetMapping("/db-test")
     public String dbTest() {
         try {
@@ -23,6 +33,11 @@ public class TestController {
         } catch (Exception e) {
             return "Conexión a la base de datos fallida: " + e.getMessage();
         }
+    }
+
+    @GetMapping("/citas-proximas")
+    public List<CitasPanel> obtenerCitasProximas() {
+        return panelDAO.obtenerCitasProximas();
     }
 
 }
---
+++ src/main/java/com/leocarlos10/backendSG_medica/conexionDAO/PanelDAO.java
@@ -0,0 +1,42 @@
+package com.leocarlos10.backendSG_medica.conexionDAO;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component; 
+import com.leocarlos10.backendSG_medica.Models.CitasPanel;
+
+@Component
+public class PanelDAO {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    private static final class CitasRowMapper implements RowMapper<CitasPanel> {
+        @Override
+        public CitasPanel mapRow(ResultSet rs, int rowNum) throws SQLException {
+            CitasPanel cita = new CitasPanel();
+            cita.setId_cita(rs.getInt("id_cita"));
+            cita.setFechaHora(rs.getString("fechaHora"));
+            cita.setEstado(rs.getString("estado"));
+            cita.setMotivoC(rs.getString("motivoC"));
+            cita.setRemitente(rs.getString("remitente"));
+            cita.setTipoCita(rs.getString("tipo_cita"));
+            cita.setNombreUsuario(rs.getString("nombre"));
+            cita.setApellidoUsuario(rs.getString("apellido"));
+            cita.setCedulaUsuario(rs.getString("cedula"));
+            return cita;
+        }
+            
+}
+    public List<CitasPanel> obtenerCitasProximas() {
+        String sql = "CALL citas_proximas()";
+        return jdbcTemplate.query(sql, new CitasRowMapper());
+    }
+
+}
